a792a7c7bf7682e402cfdf3694c3457890bf3f9e
Asier Jauregui Rivera <72439067+AsierJauregi@users.noreply.github.com>
2023-06-26 12:26:01 +0200

    Connection with Firebase app for Analytics
    Only works when trying the game through a VR headset, not in simulator

---
+++ Assets/Scripts/AnalyticsInit.cs
@@ -0,0 +1,24 @@
+using Firebase;
+using Firebase.Analytics;
+using Firebase.Extensions;
+using UnityEngine;
+
+public class AnalyticsInit : MonoBehaviour
+{
+    private Firebase.FirebaseApp app;
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                app = Firebase.FirebaseApp.DefaultInstance;
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+            }
+        });
+    }
+}
